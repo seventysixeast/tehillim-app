4da5a2eee3e476271281407d64779164d5e57411
Vishal Prajapati <31179735+DastanVishu@users.noreply.github.com>
2024-03-26 22:58:51 +0530

    app changes is done

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef, useContext} from "react";
-import {View, Text, BackHandler, Alert, Linking, LogBox} from "react-native";
+import {Animated, View, Text, BackHandler, Alert, Linking, LogBox, Image, StyleSheet} from "react-native";
 import { WebView } from 'react-native-webview';
 import SplashScreen from 'react-native-splash-screen';
 
@@ -8,6 +8,8 @@ const ViewSite = () => {
     let webviewRef = useRef(null);
     const [backinfo, setBackInfo] = useState(false);
     const [spin, setSpin] = useState(true);
+    const [translateDone, setTranslateDone] = useState(true);
+
     useEffect(()=>{
         SplashScreen.hide();
     },[])
@@ -38,13 +40,37 @@ const ViewSite = () => {
         return () => backHandler.remove();
     }, [backinfo]);
 
-    return(
-        <View
-            style={{
-                flex: 1
-            }}
-        >
+
+    const fadeValue = useRef(new Animated.Value(1)).current; // Animated value for opacity
+    const translateY = useRef(new Animated.Value(0)).current; // Animated value for Y position
+  
+    useEffect(() => {
+      Animated.parallel([ // Animate opacity and Y position simultaneously
+        Animated.timing(fadeValue, {
+          toValue: 1, // Animate to fully visible (opacity 1)
+          duration: 1000, // Animation duration in milliseconds
+        }),
+        Animated.timing(translateY, {
+          toValue: -1000, // Target Y position (adjust as needed)
+          duration: 1000, // Animation duration in milliseconds
+        }),
+      ]).start(() => {
+        setTranslateDone(false)
+      });
+    }, []);
+
+    const imageStyle = {
+        transform: [{ translateY }], // Apply animated Y position
+        opacity: fadeValue, // Apply animated opacity
+      };
+
+
+      return (
+        <View style={styles.container}>
+
+
             <WebView 
+                style={{}}
                 source={{ uri: generatedUrl }} 
                 ref={webviewRef}
                 allowsBackForwardNavigationGestures
@@ -56,7 +82,7 @@ const ViewSite = () => {
                 TouinjectedJavaScript={
                     `   
                         const meta = document.querySelector('meta[name="viewport"]');
-                        meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0';
+                        meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
                     `
                 }
 
@@ -78,9 +104,43 @@ const ViewSite = () => {
                 }}
                 // ====================================
             />
+
+            {
+                translateDone?
+                <Animated.Image 
+                    source={require("./assets/screen.png")} 
+                    style={[styles.image, imageStyle]} 
+                />:null
+            }
+            
+        </View>
+      );
+
+    return(
+        <View
+            style={{
+                flex: 1
+            }}
+        >
+
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // Occupy full space
+        position: 'absolute', // Ensure container overlaps content
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    image: {
+      width: "100%",
+      height: "100%",
+    },
+  });
 
 export default ViewSite;
