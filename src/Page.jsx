21bd22245c280b766b14d5adcbbbb79f1e46570a
Vishal Prajapati <31179735+DastanVishu@users.noreply.github.com>
2024-04-25 11:50:36 +0530

    all bug is fixed

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef, useContext} from "react";
-import {Animated, View, Text, BackHandler, Alert, Linking, LogBox, Image, StyleSheet} from "react-native";
+import {Animated, View, Text, BackHandler, Alert, Image, StyleSheet, Platform} from "react-native";
 import { WebView } from 'react-native-webview';
 import SplashScreen from 'react-native-splash-screen';
 
@@ -8,11 +8,7 @@ const ViewSite = () => {
     let webviewRef = useRef(null);
     const [backinfo, setBackInfo] = useState(false);
     const [spin, setSpin] = useState(true);
-    const [translateDone, setTranslateDone] = useState(true);
-
-    useEffect(()=>{
-        SplashScreen.hide();
-    },[])
+    const [translateDone, setTranslateDone] = useState("flex");
 
     useEffect(() => {
 
@@ -52,95 +48,102 @@ const ViewSite = () => {
         }),
         Animated.timing(translateY, {
           toValue: -1000, // Target Y position (adjust as needed)
-          duration: 1000, // Animation duration in milliseconds
+          duration: 1500, // Animation duration in milliseconds
         }),
       ]).start(() => {
-        setTranslateDone(false)
+        setTranslateDone("none")
       });
     }, []);
 
-    const imageStyle = {
-        transform: [{ translateY }], // Apply animated Y position
-        opacity: fadeValue, // Apply animated opacity
-      };
+  const imageStyle = {
+    transform: [{ translateY }], // Apply animated Y position
+    opacity: fadeValue, // Apply animated opacity
+  };
 
-
-      return (
-        <View style={styles.container}>
-
-
-            <WebView 
-                style={{}}
-                source={{ uri: generatedUrl }} 
-                ref={webviewRef}
-                allowsBackForwardNavigationGestures
-                onNavigationStateChange={(res) => {
-                    console.log("onNavigationStateChange ===>", res.url)
-                    setBackInfo(res.canGoBack)
-                }}
-
-                TouinjectedJavaScript={
-                    `   
-                        const meta = document.querySelector('meta[name="viewport"]');
-                        meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
-                    `
-                }
-
-                //Enable Javascript support
-                javaScriptEnabled={true}
-                    
-                //For the Cache
-                domStorageEnabled={true}
-                allowFileAccess={true}
-                allowFileAccessFromFileURLs={true}
-                allowingReadAccessToURL={true}
-                mixedContentMode={'always'}
-                //Want to show the view or not
-                startInLoadingState={true}
-                onLoadStart={() => setSpin(true)}
-                onLoadEnd={(e)=> {
-                    // console.log("----------===",e)
-                    setSpin(false)
-                }}
-                // ====================================
-            />
-
-            {
-                translateDone?
-                <Animated.Image 
-                    source={require("./assets/screen.png")} 
-                    style={[styles.image, imageStyle]} 
-                />:null
-            }
-            
-        </View>
-      );
-
-    return(
-        <View
-            style={{
-                flex: 1
+  return (
+    <View style={styles.container}>
+      <View style = {styles.backgroundContainer}>
+      <WebView 
+            style={Platform.OS == "ios"?{ marginBottom: 58 }:{display:"flex"}}
+            source={{ uri: generatedUrl }} 
+            ref={webviewRef}
+            allowsBackForwardNavigationGestures
+            onNavigationStateChange={(res) => {
+                console.log("onNavigationStateChange ===>", res.url)
+                setBackInfo(res.canGoBack)
             }}
-        >
 
-        </View>
-    )
+            TouinjectedJavaScript={
+                `   
+                    const meta = document.querySelector('meta[name="viewport"]');
+                    meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
+                `
+            }
+
+            //Enable Javascript support
+            javaScriptEnabled={true}
+                
+            //For the Cache
+            domStorageEnabled={true}
+            allowFileAccess={true}
+            allowFileAccessFromFileURLs={true}
+            allowingReadAccessToURL={true}
+            mixedContentMode={'always'}
+            //Want to show the view or not
+            startInLoadingState={true}
+            onLoadStart={() => setSpin(true)}
+            onLoadEnd={(e)=> {
+                // console.log("----------===",e)
+                setSpin(false)
+                SplashScreen.hide();
+            }}
+            // ====================================
+        />
+      </View>
+      <View style = {{...styles.overlay, display: translateDone }}>
+        <Animated.Image 
+          source={require("./assets/screen.png")} 
+          style={[styles.image, imageStyle]} 
+        />
+      </View>
+    </View>
+  )
 
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1, // Occupy full space
-        position: 'absolute', // Ensure container overlaps content
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-    },
-    image: {
-      width: "100%",
-      height: "100%",
-    },
-  });
+var styles = StyleSheet.create({
+  backgroundContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  overlay: {
+    width: "100%",
+  },
+  image: {
+    width: "100%"
+  },
+  logo: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    width: 160,
+    height: 52
+  },
+  backdrop: {
+    flex:1,
+    flexDirection: 'column'
+  },
+  headline: {
+    fontSize: 18,
+    textAlign: 'center',
+    backgroundColor: 'black',
+    color: 'white'
+  }
+});
 
 export default ViewSite;
